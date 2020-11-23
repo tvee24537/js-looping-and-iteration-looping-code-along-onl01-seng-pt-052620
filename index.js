0630cd9dbeadc47c1e70b1c5310525d61edfab54
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-23 18:33:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown (start) {
+    let i = start+1;
+    while (i > 0) {
+        console.log(i-=1);
+    }
+
+    return i;
+}
+
+function writeCards(names, event) {
+    let messageArray = []
+    for (let i = 0; i < names.length; i++) {
+        messageArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messageArray;
+}
